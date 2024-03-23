18ccc8a8ee786f688db841ca7b7b398eb2a01127
Chloe Nixon <93556823+ChloeNixon@users.noreply.github.com>
2024-03-23 17:44:45 -0400

    Added basic homepage skeleton, started routes
    fdsvdsvs

--- parked/Components/HomePage.js
+++ parked/Components/HomePage.js
@@ -1,9 +1,21 @@
 import {Text, View} from "react-native-web";
 import {StatusBar} from "expo-status-bar/build/StatusBar";
 
-export function HomePage(){
+const bookings = ["bbb", "ccc", "ddd"];
+
+
+export function HomePage() {
     return (<View>
         <Text>Logged In</Text>
-        <StatusBar style="auto" />
+        <StatusBar style="auto"/>
+        <h1>Available Bookings</h1>
+
+        {bookings.map((slot) => (
+            <div>{slot}</div>
+        ))}
+
+
     </View>);
 }
+
+export default HomePage;
