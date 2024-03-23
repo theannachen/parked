33be89826d25bdc9fb23af825f69c33c41a1e380
theannachen <annachen031014@gmail.com>
2024-03-23 16:55:09 -0400

    YIPPEE!!!

--- parked/App.js
+++ parked/App.js
@@ -2,10 +2,12 @@ import { RequiredAuthProvider, RedirectToLogin } from "@propelauth/react";
 import {Loading} from "./Auth/Loading"
 import {HomePage} from "./Components/HomePage"
 
-
 export default function App() {
+    console.log("URL: ");
+
+    console.log(process.env.EXPO_PUBLIC_AUTH_URL);
     return(
-    <RequiredAuthProvider authUrl={process.env.REACT_APP_AUTH_URL}>
+    <RequiredAuthProvider authUrl={process.env.EXPO_PUBLIC_AUTH_URL}>
         displayWhileLoading={<Loading />}
         displayIfLoggedOut={<RedirectToLogin />}
         <HomePage/>
